3793b365d53142a03c8efbed4bd8e3a4c654be6a
Tahiko <ruslan.losev.2012@mail.ru>
2021-10-24 23:31:42 +0300

    background

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ const concat = require('gulp-concat')
 const autoprefixer = require('gulp-autoprefixer')
 const sync = require('browser-sync').create()
 const sass = require('gulp-sass')(require('sass'))
+// const imagemin = require('gulp-imagemin')
+const image = require('gulp-image');
 
 
 function html() {
@@ -36,6 +38,13 @@ function clear() {
     return del('dist')
 }
 
+function fotochka() {
+    return src('img/*')
+        .pipe(image())
+        .pipe(dest('dist/images'))
+}
+
+
 function serve() {
     sync.init({
         server: './dist'
@@ -43,13 +52,15 @@ function serve() {
 
     watch('src/**.html', series(html)).on('change', sync.reload)
     watch('src/scss/**.scss', series(scss)).on('change', sync.reload)
+    watch('img/*',series(fotochka)).on('change',sync.reload)
 }
 
-exports.build = series(clear,scss,html)
-exports.serve = series(clear,scss,html,serve)
+exports.build = series(clear,scss,html,fotochka)
+exports.serve = series(clear,scss,html,fotochka,serve)
 exports.clear = clear
 exports.html = html
 exports.scss = scss
+
 
 
 
